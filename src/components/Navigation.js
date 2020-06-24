e83e76f73e261397c5ca366518d3c6b24e49beeb
Martuskaa <66881879+Martuskaa@users.noreply.github.com>
2020-06-24 23:06:43 +0200

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,46 +1,42 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Link
-} from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default class extends React.Component {
-  render() 
-  {return (
-    <Router>
+  render() {
+    return (
       <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
         <div className="container">
           <Link className="navbar-brand" to="/">
             Start
-        </Link>
-        <button
-          className="navbar-toggler navbar-toggler-right"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarResponsive"
-          aria-controls="navbarResponsive"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          Menu
-          <i className="fas fa-bars" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarResponsive">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-          </ul>
+          </Link>
+          <button
+            className="navbar-toggler navbar-toggler-right"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarResponsive"
+            aria-controls="navbarResponsive"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            Menu
+            <i className="fas fa-bars" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarResponsive">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
-  </Router>
-);
-}
+      </nav>
+    );
+  }
 }
